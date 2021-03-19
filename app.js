2b7706f7837ccf9affac9adde8b10f569ac5a1c8
Manten Devillé <manten.d@hotmail.com>
2021-03-19 10:02:09 +0100

    Click  events & Methods in vue

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const app = Vue.createApp({
             author: 'Brandon Sanderson',
             age: 45
         } //return object
-    } //shorthand for function inside of object : name(haakjes) {curly brackets}
+    }, //shorthand for function inside of object : name(haakjes) {curly brackets}
+    methods: {
+        changeTitle(title) {
+            //this.title =  'Words of Radiance'
+            this.title = title
+        },
+        resetTitle() {
+            this.title = 'The Final Empire'
+        }
+    }
 })
 
 app.mount('#app')
